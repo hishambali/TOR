a6cfd42d98c15dee5e2c4f035ea8b45c18eabb62
hishambali <150476592+hishambali@users.noreply.github.com>
2023-11-19 13:05:30 +0300

    Update CityModel.php
    Update CityModel.php

--- mvc/app/models/CityModel.php
+++ mvc/app/models/CityModel.php
@@ -1,18 +1,27 @@
 <?php
 // app/models/UserModel.php
+
 class CityModel {
     private $db;
 
     public function __construct($db) {
         $this->db = $db;
     }
-
-    public function getUsers() {
+    public function getCities() {
         return $this->db->get('cities');
     }
-
-    public function addUser($data) {
+    public function addCity($data) {
         return $this->db->insert('cities', $data);
     }
+    public function updateCity($id, $data) {
+        $this->db->where('id', $id);
+        return $this->db->update('cities', $data);
+    }
+    public function deleteCity($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete('cities');
+    }
+
+    
 }
 ?>
